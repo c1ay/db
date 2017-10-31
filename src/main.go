a9bd4951b1b24bb0af4ecc714a4c8d6180375933
c1ay <liukaiash@gmail.com>
2017-10-31 10:52:26 +0800

    part 2 complete newt part https://cstack.github.io/db_tutorial/parts/part3.html

--- src/main.go
+++ src/main.go
@@ -7,19 +7,72 @@ import (
 	"strings"
 )
 
-var (
-	errExist = 1
-	sucExist = 0
-	META_COMMAND_SUCCESS = 0
-	META_COMMAND_UNRECOGINZED_COMMAND = 1
+const (
+	errExist = iota
+	sucExist 
+)
+
+type metaCommandCode int
+const (
+	_ metaCommandCode = iota
+	metaCommandSuccess
+	metaCommandUnrecoginzedCommand
+)
+
+type prepareResultCode int
+const (
+	_ prepareResultCode = iota
+	prepareSuccess
+	prepareUnrecognizedStatement
 )
 
 
-
-type InputBuffer struct{
+type InputBuffer struct {
 	buffer string
 	bufferLen int
 	inputLen int
+}
+
+type statementType int
+const (
+	_ statementType = iota
+	insertType
+	selectType
+	deleteType
+)
+
+type Statement struct {
+	sType statementType
+}
+
+func prepareStatement(inputBuffer *InputBuffer, statement *Statement) prepareResultCode {
+	if len(inputBuffer.buffer) < 6 {
+		return prepareUnrecognizedStatement
+	}
+	if inputBuffer.buffer[:6] == "insert" {
+		statement.sType = insertType
+		return prepareSuccess
+	}
+	if inputBuffer.buffer[:6] == "select" {
+		statement.sType = selectType
+		return prepareSuccess
+	}
+	if inputBuffer.buffer[:6] == "delete" {
+		statement.sType = deleteType
+		return prepareSuccess
+	}
+	return prepareUnrecognizedStatement
+}
+
+func executeStatement(statement *Statement) {
+	switch statement.sType {
+	case selectType:
+		fmt.Println("do select")
+	case deleteType:
+		fmt.Println("do delete")
+	case insertType:
+		fmt.Println("do insert")
+	}
 }
 
 
@@ -29,13 +82,25 @@ func main(){
 		printPrompt()
 		readInput(inputBuffer)
 		if inputBuffer.buffer[0] == '.' {
-			switch commandRet := doMetaCommand(inputBuffer); commandRet {
-			case META_COMMAND_SUCCESS:
-				continue
-			case META_COMMAND_UNRECOGINZED_COMMAND:
-				fmt.Printf("Unrecoginzed command %s. \n", inputBuffer.buffer)
+			switch doMetaCommand(inputBuffer) {
+			case metaCommandSuccess:
+			case metaCommandUnrecoginzedCommand:
+				fmt.Printf("Unrecoginzed command '%s'. \n", inputBuffer.buffer)
 			}
 		}
+
+		// prepare statement
+		statement := new(Statement)
+		switch prepareStatement(inputBuffer, statement) {
+		case prepareSuccess:
+		case prepareUnrecognizedStatement:
+			fmt.Printf("Unrecoginzed keyword at start of '%s'. \n", inputBuffer.buffer)
+			continue
+		}
+
+		// execute statement
+		executeStatement(statement)
+		fmt.Println("Executed.")
 	}
 }
 
@@ -45,13 +110,13 @@ func printPrompt() {
 }
 
 
-func doMetaCommand(inputBuffer *InputBuffer) int {
+func doMetaCommand(inputBuffer *InputBuffer) metaCommandCode {
 	if inputBuffer.buffer == ".exit" {
 		os.Exit(sucExist)
 	} else {
-		return META_COMMAND_UNRECOGINZED_COMMAND
+		return metaCommandUnrecoginzedCommand
 	}
-	return META_COMMAND_SUCCESS
+	return metaCommandSuccess
 }
 
 func readInput(inputBuffer *InputBuffer) {
@@ -62,5 +127,5 @@ func readInput(inputBuffer *InputBuffer) {
                 fmt.Println(err)
                 os.Exit(errExist)
         }
-        inputBuffer.buffer = strings.Trim(text, "\n")
+        inputBuffer.buffer = strings.TrimSpace(text)
 }
